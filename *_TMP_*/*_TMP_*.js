2bbf1f9e3962445570f2f8acafea2472cf50fc53
matteodisabatino <matteo.di.sabatino.1989@gmail.com>
2015-04-21 19:23:30 +0200

    update application

--- *_TMP_*/*_TMP_*.js
+++ *_TMP_*/*_TMP_*.js
@@ -1,16 +1,48 @@
-var app = angular.module('ApioApplication*_TMP_*', ['apioProperty'])
-app.controller('defaultController',['$scope', 'currentObject', function($scope, currentObject){
-	console.log("Sono il defaultController e l'oggetto è")
+var app = angular.module("ApioApplication*_TMP_*", ["apioProperty"]);
+app.controller("defaultController",["$scope", "currentObject", "socket", function($scope, currentObject, socket){
+	console.log("Sono il defaultController e l'oggetto è");
 	console.log(currentObject.get());
 	$scope.object = currentObject.get();
-	/**
-	* [myCustomListener description]
-	* @return {[type]} [description]
-	*/
-	$scope.myCustomListener = function() {	
-	}
+
+    socket.on("apio_event_new", function(){
+        $scope.object.properties.fingerspread = "0";
+        $scope.object.properties.fist = "0";
+        $scope.object.properties.wavein = "0";
+        $scope.object.properties.waveout = "0";
+    });
+
+    socket.on("apio_state_new", function(){
+        $scope.object.properties.fingerspread = "0";
+        $scope.object.properties.fist = "0";
+        $scope.object.properties.wavein = "0";
+        $scope.object.properties.waveout = "0";
+    });
+
+    $scope.fingerspread = function(){
+        if(currentObject.isRecording()){
+            $("#fingerspread").find("[propertyinput]").val("1");
+        }
+    };
+
+    $scope.fist = function(){
+        if(currentObject.isRecording()){
+            $("#fist").find("[propertyinput]").val("1");
+        }
+    };
+
+    $scope.wavein = function(){
+        if(currentObject.isRecording()){
+            $("#wavein").find("[propertyinput]").val("1");
+        }
+    };
+
+    $scope.waveout = function(){
+        if(currentObject.isRecording()){
+            $("#waveout").find("[propertyinput]").val("1");
+        }
+    };
 }]);
 
-	setTimeout(function(){
-		angular.bootstrap(document.getElementById('ApioApplication*_TMP_*'), ['ApioApplication*_TMP_*']);
-	},10);
+setTimeout(function(){
+    angular.bootstrap(document.getElementById("ApioApplication*_TMP_*"), ["ApioApplication*_TMP_*"]);
+}, 10);
